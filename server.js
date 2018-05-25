93129b95c6950e4aa41d39e0049ad72e2d272a2d
Bharadwaj Pedduri <bharadwaj.p@t-hub.co>
2018-05-25 10:41:17 +0530

    Project setup complete

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static('static'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(3000, function() {
