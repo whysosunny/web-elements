0b9abd8fad0de62130a988653113a7c1ebc9e0cd
Bharadwaj Pedduri <bharadwaj.p@t-hub.co>
2018-05-29 00:22:17 +0530

    Blog setup

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+  $('#nav-link-blog').trigger('click');
+
+  //Fixed navbar on scroll
   $(window).scroll(function (e) {
     var scrollPosition = window.pageYOffset;
     var fixedBreakPoint = 360;
@@ -7,17 +10,40 @@ $(document).ready(function () {
 
     if (scrollPosition > fixedBreakPoint) {
       if (!navContainer.hasClass('top-fixed')) {
-        navContainer.addClass('top-fixed')
+        navContainer.addClass('top-fixed');
+        $('.nav-spacer').show();
       }
     } else {
       if (navContainer.hasClass('top-fixed')) {
         navContainer.removeClass('top-fixed')
+        $('.nav-spacer').hide();
       }
     }
   });
 
+
+  //Sidebar
   $('#sidebar-nav-btn').click(function() {
     $('#nav-sidebar').toggleClass('open');
-  })
+  });
+
+
+  //Navigator
+    $('.nav-item').click(function(elem) {
+      var elemId = $(this).attr('id');
+      var helperArray = elemId.split('-');
+      var pageName = helperArray[helperArray.length - 1];
+      var pageElem = $('#' + pageName);
+
+      $('.nav-item').removeClass('active');
+      $(this).addClass('active');
+
+
+      $('.page').hide();
+
+      pageElem.show();
+
+    });
+
 
 });
