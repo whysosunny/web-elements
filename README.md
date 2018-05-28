9e3a74b6f080586d3d5deef1dadd3141ff428156
Bharadwaj Pedduri <bharadwaj.p@t-hub.co>
2018-05-28 09:15:53 +0530

    Grid 1 and Fixed nav logic

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 - After cloning the repository, cd into the project directory using the terminal.
 - Download [Cmder Bash shell](http://cmder.net/)
 - Download and install Node.js [here](https://nodejs.org/en/)
+- Type `npm install` - Installing packages from 'package.json'
 - After setup from the terminal, type `node server.js`
-- Type `npm install`
 - You should see 'Server's up at http://localhost:3000' - Use it in the browser to see your website
--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,19 @@
-console.log("JS connected");
+$(document).ready(function () {
+
+  $(window).scroll(function (e) {
+    var scrollPosition = window.pageYOffset;
+    var fixedBreakPoint = 360;
+    var navContainer = $('.nav-container');
+
+    if (scrollPosition > fixedBreakPoint) {
+      if (!navContainer.hasClass('top-fixed')) {
+        navContainer.addClass('top-fixed')
+      }
+    } else {
+      if (navContainer.hasClass('top-fixed')) {
+        navContainer.removeClass('top-fixed')
+      }
+    }
+  });
+
+});
